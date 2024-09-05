d43f3d2797bd3f14f559eab64b394800f7aaac9e
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-09-05 08:37:17 -0300

    feat: adding action for submit form

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { InputsProps } from "./types";
+import useLogin from "../../hooks/useLogin";
 
 export default function Login() {
+  const [inputs, setInputs] = useState({} as InputsProps);
+  const { login, loading } = useLogin();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await login(inputs);
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0">
@@ -9,7 +20,7 @@ export default function Login() {
           <strong className="text-blue-500"> ChatApp</strong>
         </h1>
 
-          <form action="" method="post">
+          <form onSubmit={handleSubmit} method="post">
             <div>
               <label htmlFor="username" className="label p-2 text-lg font-normal">
                 Username
@@ -18,6 +29,7 @@ export default function Login() {
                 type="text" 
                 name="username" 
                 id="username" 
+                onChange={(e) => setInputs(prev => ({ ...prev, username: e.target.value}))}
                 placeholder="Enter username" 
                 className="w-full input input-bordered h-10"
               />
@@ -29,6 +41,7 @@ export default function Login() {
                 type="password" 
                 name="password" 
                 id="password" 
+                onChange={(e) => setInputs(prev => ({ ...prev, password: e.target.value}))}
                 placeholder="Enter password" 
                 className="w-full input input-bordered h-10 mb-5"
               />
@@ -37,7 +50,12 @@ export default function Login() {
               Dont´t have an account?
             </Link>
             <div>
-              <button className="btn btn-block btn-sm mt-2">Login</button>
+              <button 
+                className="btn btn-block btn-sm mt-2"
+                disabled={loading}
+              >
+                {loading ? (<span className="loading loading-spinner"></span>) : 'Login'}
+              </button>
             </div>
           </form>
       </div>
--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -113,7 +113,14 @@ export default function Signup() {
 
           <Link to="/login" className="text-sm hover:underline block mt-5">Already have an account?</Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2">Sign Up</button>
+            <button 
+              className="btn btn-block btn-sm mt-2"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : 'Sign Up'}
+            </button>
           </div>
         </form>
       </div>
